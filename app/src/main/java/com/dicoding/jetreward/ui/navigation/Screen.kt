68739084168c3583d146397afde9d508c6b30e79
Aelin Putra <alninodp@gmail.com>
2025-04-10 09:28:36 +0700

    initial commit

---
+++ app/src/main/java/com/dicoding/jetreward/ui/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.dicoding.jetreward.ui.navigation
+
+sealed class Screen (val route: String) {
+    data object Home : Screen("home")
+    data object Cart: Screen("cart")
+    data object Profile: Screen("profile")
+    data object DetailReward: Screen("home/{rewardId}"){
+        fun createRoute(rewardId: Long) = "home/$rewardId"
+    }
+}
